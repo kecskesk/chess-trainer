32a8c464d528742ed96fb9d31144e5a6e6d14d1c
Kecskes Krisztian <kecskesk@sonrisa.hu>
2023-08-19 19:46:35 +0200

    candrop?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,26 +15,50 @@ export class AppComponent implements AfterViewInit {
   dropLists: CdkDropList[] = undefined;
   title = 'chess-trainer';
   field = [
-    [[new ChessPieceDto('white', 'pawn')], [], [], [], [], [], [], []],
-    [[], [], [], [new ChessPieceDto('white', 'pawn')], [], [], [], []],
+    [[new ChessPieceDto('black', 'rook')], [new ChessPieceDto('black', 'knight')], [new ChessPieceDto('black', 'bishop')], [new ChessPieceDto('black', 'queen')], [new ChessPieceDto('black', 'king')], [new ChessPieceDto('black', 'bishop')], [new ChessPieceDto('black', 'knight')], [new ChessPieceDto('black', 'rook')]],
+    [[new ChessPieceDto('black', 'pawn')], [new ChessPieceDto('black', 'pawn')], [new ChessPieceDto('black', 'pawn')], [new ChessPieceDto('black', 'pawn')], [new ChessPieceDto('black', 'pawn')], [new ChessPieceDto('black', 'pawn')], [new ChessPieceDto('black', 'pawn')], [new ChessPieceDto('black', 'pawn')]],
     [[], [], [], [], [], [], [], []],
     [[], [], [], [], [], [], [], []],
     [[], [], [], [], [], [], [], []],
     [[], [], [], [], [], [], [], []],
-    [[new ChessPieceDto('white', 'pawn')], [], [], [], [], [], [], []],
-    [[], [], [], [], [], [], [], []]
+    [[new ChessPieceDto('white', 'pawn')], [new ChessPieceDto('white', 'pawn')], [new ChessPieceDto('white', 'pawn')], [new ChessPieceDto('white', 'pawn')], [new ChessPieceDto('white', 'pawn')], [new ChessPieceDto('white', 'pawn')], [new ChessPieceDto('white', 'pawn')], [new ChessPieceDto('white', 'pawn')]],
+    [[new ChessPieceDto('white', 'rook')], [new ChessPieceDto('white', 'knight')], [new ChessPieceDto('white', 'bishop')], [new ChessPieceDto('white', 'queen')], [new ChessPieceDto('white', 'king')], [new ChessPieceDto('white', 'bishop')], [new ChessPieceDto('white', 'knight')], [new ChessPieceDto('white', 'rook')]]
   ];
 
   ngAfterViewInit(): void {
     if (this.dropListElements) {
       setTimeout(() => {
         this.dropLists = this.dropListElements.toArray();
+        window['field'] = this.field;
       }, 0);
     }
   }
 
-  canDrop(item: CdkDrag<ChessPieceDto[]>, list: CdkDropList<ChessPieceDto[]>): boolean {
-    return list.data.length < 2;
+  canDrop(drag: CdkDrag<ChessPieceDto[]>, drop: CdkDropList<ChessPieceDto[]>): boolean {
+    let targetObj = null;
+    let targetPiece = null;
+    let targetColor = null;
+    if (drop && drop.data && drop.data[0]) {
+      targetObj = drop.data[0];
+      targetColor = targetObj.color;
+      targetPiece = targetObj.piece;
+    }
+    const targetLoc = drop.id;
+    const sourceLocation = drag.dropContainer.id;
+    let sourceObj = null;
+    let sourcePiece = null;
+    let sourceColor = null;
+    const sourceLocSplit = sourceLocation.split('field');
+    const sourceRow = sourceLocSplit[1][0];
+    const sourceCell = sourceLocSplit[1][0];
+    if (window['field']) {
+      sourceObj = window['field'][sourceRow][sourceCell];
+      if (sourceObj && sourceObj[0]) {
+        sourceColor = sourceObj[0].color;
+        sourcePiece = sourceObj[0].piece;
+      }
+    }
+    return drop.data.length < 2;
   }
 
   onDrop(event: CdkDragDrop<ChessPieceDto[]>) {
