344b93695461a12d210f236a5009329ece6c4d74
Kecskes Krisztian <kecskesk@sonrisa.hu>
2023-08-19 21:00:47 +0200

    steps with no hits, no collision

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,7 @@ export class AppComponent implements AfterViewInit {
   @ViewChildren(CdkDropList) dropListElements: QueryList<CdkDropList>;
 
   dropLists: CdkDropList[] = undefined;
+  debugObj = { debugText: '', possibles: [] };
   title = 'chess-trainer';
   field = [
     [[new ChessPieceDto('black', 'rook')], [new ChessPieceDto('black', 'knight')], [new ChessPieceDto('black', 'bishop')], [new ChessPieceDto('black', 'queen')], [new ChessPieceDto('black', 'king')], [new ChessPieceDto('black', 'bishop')], [new ChessPieceDto('black', 'knight')], [new ChessPieceDto('black', 'rook')]],
@@ -30,6 +31,7 @@ export class AppComponent implements AfterViewInit {
       setTimeout(() => {
         this.dropLists = this.dropListElements.toArray();
         window['field'] = this.field;
+        window['debugObj'] = this.debugObj;
       }, 0);
     }
   }
@@ -44,13 +46,16 @@ export class AppComponent implements AfterViewInit {
       targetPiece = targetObj.piece;
     }
     const targetLoc = drop.id;
+    const targetLocSplit = targetLoc.split('field');
+    const targetRow = Number(targetLocSplit[1][0]);
+    const targetCell = Number(targetLocSplit[1][1]);
     const sourceLocation = drag.dropContainer.id;
     let sourceObj = null;
     let sourcePiece = null;
     let sourceColor = null;
     const sourceLocSplit = sourceLocation.split('field');
-    const sourceRow = sourceLocSplit[1][0];
-    const sourceCell = sourceLocSplit[1][0];
+    const sourceRow = Number(sourceLocSplit[1][0]);
+    const sourceCell = Number(sourceLocSplit[1][1]);
     if (window['field']) {
       sourceObj = window['field'][sourceRow][sourceCell];
       if (sourceObj && sourceObj[0]) {
@@ -58,10 +63,72 @@ export class AppComponent implements AfterViewInit {
         sourcePiece = sourceObj[0].piece;
       }
     }
-    return drop.data.length < 2;
+    let canDrop = drop.data.length < 1;
+    switch (sourcePiece) {
+      case 'pawn': {
+        const stepY = targetRow - sourceRow;
+        const targetDirectionStep = sourceColor === 'white' ? -1 : 1;
+        const homeRow = sourceColor === 'white' ? 6 : 1;
+        const homeRowStep = sourceColor === 'white' ? -2 : 2;
+        const sameColumn = targetCell === sourceCell;
+        if (!sameColumn || !((stepY === targetDirectionStep) || (sourceRow == homeRow && stepY === homeRowStep))) {
+          canDrop = false;
+        }
+        break;
+      }
+      case 'knight': {
+        const stepX = Math.abs(targetCell - sourceCell);
+        const stepY = Math.abs(targetRow - sourceRow);
+        if (!(stepX === 2 && stepY === 1) && !(stepX === 1 && stepY === 2)) {
+          canDrop = false;
+        }
+        break;
+      }
+      case 'king': {
+        if (Math.abs(targetCell - sourceCell) > 1) {
+          canDrop = false;
+        }
+        if (Math.abs(targetRow - sourceRow) > 1) {
+          canDrop = false;
+        }
+        break;
+      }
+      case 'queen': {
+        if (Math.abs(targetCell - sourceCell) !== Math.abs(targetRow - sourceRow) && targetCell !== sourceCell && targetRow !== sourceRow) {
+          canDrop = false;
+        }
+        break;
+      }
+      case 'rook': {
+        if (targetCell !== sourceCell && targetRow !== sourceRow) {
+          canDrop = false;
+        }
+        break;
+      }
+      case 'bishop': {
+        if (Math.abs(targetCell - sourceCell) !== Math.abs(targetRow - sourceRow)) {
+          canDrop = false;
+        }
+        break;
+      }
+      default:
+        break;
+    }
+    if (window['debugObj'] && canDrop) {
+      // window['debugObj'].debugText += `source c${sourceCell}r${sourceRow}`
+      const letterChar = String.fromCharCode('a'.charCodeAt(0) + targetCell);
+      const numberChar = (8 - targetRow);
+      if (!window['debugObj'].possibles) {
+        window['debugObj'].possibles = [];
+      }
+      window['debugObj'].possibles.push(targetLoc)
+    }
+    return canDrop;
   }
 
   onDrop(event: CdkDragDrop<ChessPieceDto[]>) {
+    window['debugObj'].debugText = '';
+    window['debugObj'].possibles = [];
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data,
         event.previousIndex,
@@ -71,5 +138,15 @@ export class AppComponent implements AfterViewInit {
         event.container.data,
         event.previousIndex, event.currentIndex);
     }
+  }
+
+  isTarget(id: string) {
+    return this.debugObj.possibles && this.debugObj.possibles.includes(id);
+  }
+
+  translate(idxX: number, idxY: number) {
+    const letterChar = String.fromCharCode('a'.charCodeAt(0) + idxY);
+    const numberChar = (8 - idxX);
+    return `${letterChar}${numberChar}`
   }
 }
