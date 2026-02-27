35152ee7500a0b8aa42cae752dbcd4c219e48430
Kecskés Krisztián <kecskesk@sonrisa.hu>
2026-02-27 14:27:53 +0100

    Fix jasmine, add some tests

---
+++ src/app/services/global-variables.service.spec.ts
@@ -0,0 +1,26 @@
+import { GlobalVariablesService } from './global-variables.service';
+import { ChessPiecesEnum } from '../model/chess.pieces';
+
+describe('GlobalVariablesService notation helpers', () => {
+  it('translates piece notation correctly', () => {
+    expect(GlobalVariablesService.translatePieceNotation(ChessPiecesEnum.Knight)).toBe('N');
+    expect(GlobalVariablesService.translatePieceNotation(ChessPiecesEnum.Pawn)).toBe('');
+  });
+
+  it('builds move notation with capture and check', () => {
+    const notation = GlobalVariablesService.translateNotation(
+      4,
+      4,
+      6,
+      4,
+      ChessPiecesEnum.Knight,
+      true,
+      true,
+      false,
+      false,
+      null
+    );
+
+    expect(notation).toBe('Ne2xe4+');
+  });
+});
